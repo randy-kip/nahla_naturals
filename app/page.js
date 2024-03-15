2fcfce14387e8cde8063dad694bf7ea7bffab113
Randy Kipkurui <randy.kipkurui4@gmail.com>
2024-03-15 21:16:37 +0300

    💄style: add styling to the products section
    ✨feat: create, export and import components to be used later on.

--- app/page.js
+++ app/page.js
@@ -1,16 +1,18 @@
 import React from "react";
 
+import { Product, FooterBanner, HeroBanner } from '../components'
+
 const Home = () => {
   return (
     <>
       HeroBanner
-
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Essential Oils to fit your Lifestyle</p>
       </div>
-      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
-
+      <div className="products-container">
+        {["Product 1", "Product 2"].map((product) => product)}
+      </div>
       Footer
     </>
   );
