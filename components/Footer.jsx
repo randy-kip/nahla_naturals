ef34a3bc3394bc1ae32cfc484200b1f1101bfded
Randy Kipkurui <randy.kipkurui4@gmail.com>
2024-03-25 17:30:11 +0300

    💄style: add some styling and content to the Navbar and Footer components.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer-container">
+      <p>2024 Nahla Naturals All Rights Reserved</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
