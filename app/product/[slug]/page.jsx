d5315b2ca8f6af2f54b6e1d8a90fd0928fe93b28
Randy Kipkurui <randy.kipkurui4@gmail.com>
2024-03-26 14:01:41 +0300

    ✨feat: fetch product data from Sanity.
    💩fix:the code needs to be fixed to comply with nextjs requirements for fetching data and dynamic routing.

--- app/product/[slug]/page.jsx
+++ app/product/[slug]/page.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import { client, urlFor } from "@/lib/client";
 
-const ProductDetails = () => {
+const ProductDetails = ({ product, productsQuery }) => {
+    const { image, name, details, price } = product;
+
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src="" />
+            <img src={urlFor(image && image[0]).url()} />
           </div>
         </div>
       </div>
     </div>
   );
 };
+
+export async function getProducts({ params: { slug } }) {
+  const product = await client.fetch(
+    `*[_type == "product" && slug.current == '${slug}'][0]`
+  );
+
+  const productsQuery = await client.fetch('*[_type == "product"]');
+
+  console.log(product);
+  return { product, productsQuery };
+}
 
 export default ProductDetails;
