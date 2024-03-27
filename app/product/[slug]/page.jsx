a57ebcad7b60127cf4916be298f191df9804b6f6
Randy Kipkurui <randy.kipkurui4@gmail.com>
2024-03-27 18:34:56 +0300

    💩fix: add commented out code to help me fix data fetching bug.

--- app/product/[slug]/page.jsx
+++ app/product/[slug]/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { client, urlFor } from "@/lib/client";
 
 const ProductDetails = ({ product, productsQuery }) => {
-    const { image, name, details, price } = product;
+  const { image, name, details, price } = product;
 
   return (
     <div>
@@ -17,7 +17,34 @@ const ProductDetails = ({ product, productsQuery }) => {
   );
 };
 
-export async function getProducts({ params: { slug } }) {
+export async function getStaticPaths() {
+  const query = `*[_type == "product"] {
+    slug {
+      current
+    }
+  }`;
+
+  // const client = getClient(); // Obtain your Sanity client instance
+  const products = await client.fetch(query);
+
+  const paths = products.map((product) => ({
+    params: { 
+      slug: product.slug.current
+    }
+  }));
+
+  return {
+    paths,
+    fallback: 'blocking'
+  };
+}
+
+// export async function getProducts() {
+//   const products = await client.fetch('*[_type == "product"]');
+//   return products;
+// }
+
+export async function getProductDetails({ params: { slug } }) {
   const product = await client.fetch(
     `*[_type == "product" && slug.current == '${slug}'][0]`
   );
@@ -27,5 +54,19 @@ export async function getProducts({ params: { slug } }) {
   console.log(product);
   return { product, productsQuery };
 }
+
+// export const getStaticProps = async ({ params: { slug } }) => {
+//   const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+//   const productsQuery = '*[_type == "product"]';
+
+//   const product = await client.fetch(query);
+//   const products = await client.fetch(productsQuery);
+
+//   console.log(product);
+
+//   return {
+//     props: { products, product },
+//   };
+// };
 
 export default ProductDetails;
